272e5e0f3b00db9f7c280a9ae25d75a0018d7ce5
Hermes Júnior <hermesj.junior@gmail.com>
2022-07-15 09:19:16 -0300

    routes adjustment

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import "express-async-errors";
 import dotenv from "dotenv";
 dotenv.config();
 
-//import router from "./routers/index.js";
-//import errorHandler from "./middlewares/errorHandlerMiddleware.js";
+import router from "./routes/index.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
 app.use(json());
-//app.use(router);
-//app.use(errorHandler);
+app.use(router);
+app.use(errorHandler);
 
 const port = +process.env.PORT || 5000;
 app.listen(port, () => {
